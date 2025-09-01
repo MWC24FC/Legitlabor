1a4894bdee53f704bb8927ae20c7c75afb574700
MWC24FC <ethan.ezra.starke@gmail.com>
2025-08-31 21:58:09 -0700

    Create testUsers.ts

---
+++ tests/e2e/fixtures/testUsers.ts
@@ -0,0 +1,5 @@
+export const users = {
+  employer: { email: 'employer+e2e@legitlabor.dev', password: 'Passw0rd!e2e' },
+  worker:   { email: 'worker+e2e@legitlabor.dev',   password: 'Passw0rd!e2e' },
+  admin:    { email: 'admin+e2e@legitlabor.dev',    password: 'Passw0rd!e2e' },
+};
